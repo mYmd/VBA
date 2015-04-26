1fcc8d1358b923c3d4c6bcbb329fa450d4e69d72
mYmd <m-yamada@f06.itscom.net>
2015-04-26 09:31:49 +0900

    リファクタリング

--- VBA_NestFunc.hpp
+++ VBA_NestFunc.hpp
@@ -3,14 +3,6 @@
 
 #include <memory>
 
-
-//使用する唯一のVBAコールバック関数型  VBCallbackFunc  の宣言
-//VBAにおけるシグネチャは
-// Function fun(ByRef elem As Variant, ByRef dummy As Variant) As Variant
-// もしくは
-// Function fun(ByRef elem As Variant, Optional ByRef dummy As Variant) As Variant
-typedef VARIANT (__stdcall *VBCallbackFunc)(VARIANT*, VARIANT*);
-
 //VBA配列の次元取得
 __int32 __stdcall   Dimension(const VARIANT* pv);
 
@@ -29,37 +21,40 @@ VARIANT __stdcall   bind_invoke(VARIANT* bfun, VARIANT* param);
 //要素数とLBoundを取得
 void    safeArrayBounds(SAFEARRAY* pArray, UINT dim, SAFEARRAYBOUND bounds[]);
 
-VBCallbackFunc  get_bindFun(const VARIANT* bfun, VARIANT& elem1, VARIANT& elem2);
-
 //--------------------------------------------------------
 class funcExpr_i    {
 public:
     virtual ~funcExpr_i();
     virtual VARIANT* eval(VARIANT*, VARIANT*) = 0;
 };
-//--------------------------------------------------------
-class valueExpr : public funcExpr_i    {
-    VARIANT     val;
-public:
-    valueExpr(VARIANT* v);
-    ~valueExpr();
-    VARIANT* eval(VARIANT*, VARIANT*);
-};
-//--------------------------------------------------------
-class placeholderExpr : public funcExpr_i    {
-public:
-    ~placeholderExpr();
-    VARIANT* eval(VARIANT*, VARIANT*);
-};
+
 //--------------------------------------------------------
 class functionExpr : public funcExpr_i    {
-    VBCallbackFunc  fun;
+    //使用する唯一のVBAコールバック関数型  vbCallbackFunc_t
+    //VBAにおけるシグネチャは
+    // Function fun(ByRef elem As Variant, ByRef dummy As Variant) As Variant
+    // もしくは
+    // Function fun(ByRef elem As Variant, Optional ByRef dummy As Variant) As Variant
+    typedef VARIANT (__stdcall *vbCallbackFunc_t)(VARIANT*, VARIANT*);
+    vbCallbackFunc_t  fun;
     VARIANT         val;
     std::unique_ptr<funcExpr_i> left;
     std::unique_ptr<funcExpr_i> right;
 public:
-    functionExpr(VBCallbackFunc, VARIANT&, VARIANT&);
+        class VBCallbackFunc_   {
+            friend class functionExpr;
+            vbCallbackFunc_t    fun;
+            VARIANT             elem1;
+            VARIANT             elem2;
+        public:
+            VBCallbackFunc_(const VARIANT* bfun);
+            ~VBCallbackFunc_();
+            operator bool() const;
+        };
+    //---------------------------------
+    functionExpr(VBCallbackFunc_&);
     ~functionExpr();
     VARIANT* eval(VARIANT*, VARIANT*);
 };
 
+using VBCallbackFunc = functionExpr::VBCallbackFunc_;
