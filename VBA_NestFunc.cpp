18552afaa9c5f831a460c7783202116f1585a6a9
mYmd <m-yamada@f06.itscom.net>
2015-05-16 01:52:25 +0900

    プレースホルダ追加対応

--- VBA_NestFunc.cpp
+++ VBA_NestFunc.cpp
@@ -17,19 +17,19 @@ __int32 __stdcall Dimension(const VARIANT* pv)
 }
 
 //プレースホルダの生成
-VARIANT __stdcall placeholder()
+VARIANT __stdcall placeholder(__int32 n)
 {
     VARIANT ret;
     ::VariantInit(&ret);
     ret.vt = VT_ERROR;
-    ret.scode = 0;
+    ret.scode = n;
     return ret;
 }
 
 //プレースホルダの判定
 __int32 __stdcall is_placeholder(const VARIANT* pv)
 {
-    return ( pv && (pv->vt == VT_ERROR) && pv->scode == 0 ) ? 1 : 0;
+    return ( pv && (pv->vt == VT_ERROR) && 0 <= pv->scode && pv->scode <= 2 ) ? 1 : 0;
 }
 
 //要素数とLBoundを取得
@@ -58,16 +58,40 @@ public:
         ::VariantCopy(&val, v);
     }
     ~valueExpr()    {   }
-    VARIANT* eval(VARIANT*, VARIANT*)   {   return &val;    }
+    VARIANT* eval(VARIANT*, VARIANT*, int left_right = 0)   {   return &val;    }
 };
 
 //--------------------------------------------------------
-class placeholderExpr : public funcExpr_i    {
+class placeholder0 : public funcExpr_i    {
 public:
-    ~placeholderExpr()  {   }
-    VARIANT* eval(VARIANT* x, VARIANT*) {   return x;   }
+    ~placeholder0()  {   }
+    VARIANT* eval(VARIANT* x, VARIANT* y, int left_right = 0)
+    {
+        return ( left_right == 1 )?  x: ( left_right == 2 )?  y:  0;
+    }
 };
 
+class placeholder1 : public funcExpr_i    {
+public:
+    ~placeholder1()  {   }
+    VARIANT* eval(VARIANT* x, VARIANT* y, int left_right = 0)
+    {   return x;   }
+};
+
+class placeholder2 : public funcExpr_i    {
+public:
+    ~placeholder2()  {   }
+    VARIANT* eval(VARIANT* x, VARIANT* y, int left_right = 0)
+    {   return y;   }
+};
+
+//-------------------------------------------------------------
+namespace   {
+    int placeholder_num(const VARIANT* pv)
+    {
+        return ( pv && (pv->vt == VT_ERROR) ) ?  pv->scode :  -1;
+    }
+}
 //-------------------------------------------------------------
 functionExpr::VBCallbackFunc_::VBCallbackFunc_(const VARIANT* bfun) : fun(0)
 {
@@ -84,9 +108,9 @@ functionExpr::VBCallbackFunc_::VBCallbackFunc_(const VARIANT* bfun) : fun(0)
         VARIANT elem3;
         ::VariantInit(&elem3);
         ::SafeArrayGetElement(pArray, &index, &elem3);
-        auto isP = is_placeholder(&elem3);
+        auto isP = placeholder_num(&elem3);
         ::VariantClear(&elem3);
-        if ( !isP )                     return;
+        if ( isP < 0 )                     return;
     }
     {
         VARIANT elem0;
@@ -126,19 +150,29 @@ functionExpr::functionExpr(VBCallbackFunc_& callback) : fun(callback.fun)
     ::VariantInit(&val);
     {
         VBCallbackFunc_ fun1(&callback.elem1);
+        int pn = -1;
         if ( fun1 )
             left.reset(new functionExpr(fun1));
-        else if ( is_placeholder(&callback.elem1) )
-            left.reset(new placeholderExpr);
+        else if ( 0 == (pn = placeholder_num(&callback.elem1)) )
+            left.reset(new placeholder0);
+        else if ( 1 == pn )
+            left.reset(new placeholder1);
+        else if ( 2 == pn )
+            left.reset(new placeholder2);
         else
             left.reset(new valueExpr(&callback.elem1));
     }
     {
         VBCallbackFunc_ fun2(&callback.elem2);
+        int pn = -1;
         if ( fun2 )
             right.reset(new functionExpr(fun2));
-        else if ( is_placeholder(&callback.elem2) )
-            right.reset(new placeholderExpr);
+        else if ( 0 == (pn = placeholder_num(&callback.elem2)) )
+            right.reset(new placeholder0);
+        else if ( 1 == pn )
+            right.reset(new placeholder1);
+        else if ( 2 == pn )
+            right.reset(new placeholder2);
         else
             right.reset(new valueExpr(&callback.elem2));
     }
@@ -149,11 +183,12 @@ functionExpr::~functionExpr()
     ::VariantClear(&val);
 }
 
-VARIANT* functionExpr::eval(VARIANT* x, VARIANT* y)
+VARIANT* functionExpr::eval(VARIANT* x, VARIANT* y, int left_right) // = 0
 {
     if ( fun )
     {
-        VARIANT tmp = fun(left->eval(x, x), right->eval(y, y));
+        VARIANT tmp = fun(  left->eval(x, y, left_right? left_right : 1),
+                           right->eval(x, y, left_right? left_right : 2)    );
         ::VariantClear(&val);
         std::swap(val, tmp);
     }
