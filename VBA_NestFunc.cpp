24d4674053a616d057a19bc169dc92bae24e7e3a
mYmd <m-yamada@f06.itscom.net>
2015-06-09 00:02:37 +0900

    safearrayRef no

--- VBA_NestFunc.cpp
+++ VBA_NestFunc.cpp
@@ -31,15 +31,67 @@ __int32 __stdcall is_placeholder(const VARIANT* pv)
     return ( pv && (pv->vt == VT_ERROR) && 0 <= pv->scode && pv->scode <= 2 ) ? 1 : 0;
 }
 
-//要素数とLBoundを取得
-void safeArrayBounds(SAFEARRAY* pArray, UINT dim, SAFEARRAYBOUND bounds[3])
+//===================================================================
+safearrayRef::safearrayRef(const VARIANT* pv)
+    :psa(nullptr), pvt(0), dim(0), elemsize(0), it(nullptr)//, size{1,1,1}
 {
-    for ( ULONG i = 0; i < dim; ++i )
+    ::VariantInit(&val_);
+    if (!pv || 0 == (VT_ARRAY & pv->vt))            return;
+    psa = (0 == (VT_BYREF & pv->vt))? pv->parray: *pv->pparray;
+    if (!psa)                                       return;
+    SafeArrayAccessData(psa, reinterpret_cast<void**>(&it));
+    dim = SafeArrayGetDim(psa);
+    if (!it || 3 < dim)
     {
-        ::SafeArrayGetLBound(pArray, i+1, &bounds[i].lLbound);
-        LONG ub = 0;
-        ::SafeArrayGetUBound(pArray, i+1, &ub);
-        bounds[i].cElements = 1 + ub - bounds[i].lLbound;
+        dim = 0;
+        return;
+    }
+    elemsize = SafeArrayGetElemsize(psa);
+    SafeArrayGetVartype(psa, &pvt);
+    val_.vt = pvt | VT_BYREF;   //ここ
+    size[0] = size[1] = size[2] = 1;
+    for (decltype(dim) i = 0; i < dim; ++i)
+    {
+        LONG ub = 0, lb = 0;
+        ::SafeArrayGetUBound(psa, i+1, &ub);
+        ::SafeArrayGetLBound(psa, i+1, &lb);
+        size[i] = ub - lb + 1;
+    }
+}
+safearrayRef::~safearrayRef()
+{
+    if(psa)     SafeArrayUnaccessData(psa);
+    VariantClear(&val_);
+}
+
+std::size_t safearrayRef::getDim() const
+{
+    return dim;
+}
+
+std::size_t safearrayRef::getSize(std::size_t i) const
+{
+    return size[i-1];
+}
+
+std::size_t safearrayRef::getOriginalLBound(std::size_t i) const
+{
+    LONG lb = 0;
+    ::SafeArrayGetLBound(psa, i, &lb);
+    return lb;
+}
+
+VARIANT& safearrayRef::operator()(std::size_t i, std::size_t j, std::size_t k)
+{
+    auto distance = size[0]*size[1]*k + size[0]*j + i;
+    if ( pvt == VT_VARIANT)
+    {
+        return *reinterpret_cast<VARIANT*>(it + distance*elemsize);
+    }
+    else
+    {
+        val_.pvarVal = reinterpret_cast<VARIANT*>(it + distance*elemsize);
+        return val_;
     }
 }
 
@@ -96,40 +148,15 @@ functionExpr::VBCallbackFunc_::VBCallbackFunc_(const VARIANT* bfun) : fun(0)
 {
     ::VariantInit(&elem1);
     ::VariantInit(&elem2);
-    if ( 1 != Dimension(bfun) )         return;
-    SAFEARRAY* pArray = ( 0 == (VT_BYREF & bfun->vt) )?  (bfun->parray): (*bfun->pparray);
-    if ( !pArray )                      return;
-    SAFEARRAYBOUND bounds = {1,0};   //要素数、LBound
-    safeArrayBounds(pArray, 1, &bounds);
-    if ( bounds.cElements < 4 )         return;
-    LONG index = bounds.lLbound + 3;
-    {
-        VARIANT elem3;
-        ::VariantInit(&elem3);
-        ::SafeArrayGetElement(pArray, &index, &elem3);
-        auto isP = placeholder_num(&elem3);
-        ::VariantClear(&elem3);
-        if ( isP < 0 )                     return;
-    }
-    {
-        VARIANT elem0;
-        ::VariantInit(&elem0);
-        index = bounds.lLbound + 0;
-        ::SafeArrayGetElement(pArray, &index, &elem0);
-        if ( elem0.vt != VT_I4 || elem0.lVal == 0 )
-        {
-            ::VariantClear(&elem0);
-            return;
-        }
-        fun = reinterpret_cast<vbCallbackFunc_t>(elem0.lVal);
-        ::VariantClear(&elem0);
-    }
-    {
-        index = bounds.lLbound + 1;
-        ::SafeArrayGetElement(pArray, &index, &elem1);
-        index = bounds.lLbound + 2;
-        ::SafeArrayGetElement(pArray, &index, &elem2);
-    }
+    safearrayRef arRef(bfun);
+    if ( 1 != arRef.getDim() )         return;
+    if ( arRef.getSize(1) < 4 )        return;
+    if ( placeholder_num(&arRef(3)) < 0 )   return;
+    VARIANT& elem0 = arRef(0);
+    if ( elem0.vt != VT_I4 || elem0.lVal == 0 ) return;
+    fun = reinterpret_cast<vbCallbackFunc_t>(elem0.lVal);
+    ::VariantCopy(&elem1, &arRef(1));
+    ::VariantCopy(&elem2, &arRef(2));
 }
 
 functionExpr::VBCallbackFunc_::~VBCallbackFunc_()
