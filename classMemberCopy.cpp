47cc8d45d0a1a2e85a02bdaaefcf89d784ba5e3c
mYmd <m-yamada@f06.itscom.net>
2016-12-13 21:11:04 +0900

    唯のリファクタリング

--- classMemberCopy.cpp
+++ classMemberCopy.cpp
@@ -6,7 +6,31 @@
 
 namespace   {
     //ReDim可能な配列かどうか
-    HRESULT redimCheck(SAFEARRAY * psa);
+    HRESULT redimCheck(SAFEARRAY * psa)
+    {
+        if ( const auto dim = ::SafeArrayGetDim(psa) )
+        {
+            SAFEARRAYBOUND saboundNew{ 0, 0 };
+            LONG lLbound, lUbound;
+            auto a1 = ::SafeArrayGetLBound(psa, dim, &lLbound);
+            auto a2 = ::SafeArrayGetUBound(psa, dim, &lUbound);
+            saboundNew.cElements = 1 + lUbound - lLbound;
+            saboundNew.lLbound = lLbound;
+            return ::SafeArrayRedim(psa, &saboundNew);
+        }
+        return S_OK;    //未初期化配列もReDim可能
+    }
+
+    // コピー  *me->mem   <===>   *target->mem
+    template <typename T>
+    void copy_or_swap(T* pV, IDispatch* me, IDispatch* target, __int32 dir)
+    {
+        auto n = reinterpret_cast<char*>(pV) - reinterpret_cast<char*>(me);
+        auto p = reinterpret_cast<T*>(reinterpret_cast<char*>(target) + n);
+        if (0 < dir)        *p = *pV;
+        else if (dir < 0)   *pV = *p;
+        else                std::swap(*p, *pV);
+    }
 }
 
 // VBAのクラスオブジェクトの特定のメンバ（オブジェクト型以外）を同一クラスの他のオブジェクトにコピーする
@@ -61,106 +85,29 @@ VARIANT_BOOL __stdcall copy_valueMember(IDispatch* me, VARIANT* mbr, IDispatch* 
                     ::SafeArrayDestroy(tmp);
                 }
             }
-            return -1;
         }
         else switch ( vt % VT_ARRAY )
         {
-            case    VT_UI1:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pbVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<BYTE*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pbVal; }
-                else if( dir < 0 )  { *mbr->pbVal = *p; }
-                else                { std::swap(*p, *mbr->pbVal); }
-                break;
-            }
-            case    VT_I2:
-            {
-                auto n = reinterpret_cast<char*>(mbr->piVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<SHORT*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->piVal; }
-                else if ( dir < 0 ) { *mbr->piVal = *p; }
-                else                { std::swap(*p, *mbr->piVal); }
-                break;
-            }
-            case    VT_I4:
-            {
-                auto n = reinterpret_cast<char*>(mbr->plVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<LONG*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->plVal; }
-                else if ( dir < 0 ) { *mbr->plVal = *p; }
-                else                { std::swap(*p, *mbr->plVal); }
-                break;
-            }
-            case    VT_I8:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pllVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<LONGLONG*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pllVal; }
-                else if ( dir < 0 ) { *mbr->pllVal = *p; }
-                else                { std::swap(*p, *mbr->pllVal); }
-                break;
-            }
-            case    VT_R4:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pfltVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<FLOAT*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pfltVal; }
-                else if ( dir < 0 ) { *mbr->pfltVal = *p; }
-                else                { std::swap(*p, *mbr->pfltVal); }
-                break;
-            }
-            case    VT_R8:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pdblVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<DOUBLE*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pdblVal; }
-                else if ( dir < 0 ) { *mbr->pdblVal = *p; }
-                else                { std::swap(*p, *mbr->pdblVal); }
-                break;
-            }
-            case    VT_CY:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pcyVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<CY*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pcyVal; }
-                else if ( dir < 0 ) { *mbr->pcyVal = *p; }
-                else                { std::swap(*p, *mbr->pcyVal); }
-                break;
-            }
-            case    VT_DATE:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pdate) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<DATE*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pdate; }
-                else if ( dir < 0 ) { *mbr->pdate = *p; }
-                else                { std::swap(*p, *mbr->pdate); }
-                break;
-            }
-            case    VT_BSTR:
+            case    VT_UI1:     copy_or_swap(mbr->pbVal, me, target, dir);      break;
+            case    VT_I2:      copy_or_swap(mbr->piVal, me, target, dir);      break;
+            case    VT_I4:      copy_or_swap(mbr->plVal, me, target, dir);      break;
+            case    VT_I8:      copy_or_swap(mbr->pllVal, me, target, dir);     break;
+            case    VT_R4:      copy_or_swap(mbr->pfltVal, me, target, dir);    break;
+            case    VT_R8:      copy_or_swap(mbr->pdblVal, me, target, dir);    break;
+            case    VT_CY:      copy_or_swap(mbr->pcyVal, me, target, dir);     break;
+            case    VT_DATE:    copy_or_swap(mbr->pdate, me, target, dir);      break;
+            case    VT_BOOL:    copy_or_swap(mbr->pboolVal, me, target, dir);   break;
+            case    VT_BSTR:    /////////////
             {
                 auto n = reinterpret_cast<char*>(mbr->pbstrVal) - reinterpret_cast<char*>(me);
                 auto p = reinterpret_cast<BSTR*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { ::SysReAllocString(p, *mbr->pbstrVal); }
-                else if ( dir < 0 ) { ::SysReAllocString(mbr->pbstrVal, *p); }
-                else                { std::swap(*p, *mbr->pbstrVal); }
+                if ( 0 < dir )          ::SysReAllocString(p, *mbr->pbstrVal);
+                else if ( dir < 0 )     ::SysReAllocString(mbr->pbstrVal, *p);
+                else                    std::swap(*p, *mbr->pbstrVal);
                 break;
             }
-            case    VT_BOOL:
-            {
-                auto n = reinterpret_cast<char*>(mbr->pboolVal) - reinterpret_cast<char*>(me);
-                auto p = reinterpret_cast<VARIANT_BOOL*>(reinterpret_cast<char*>(target) + n);
-                if ( 0 < dir )      { *p = *mbr->pboolVal; }
-                else if ( dir < 0 ) { *mbr->pboolVal = *p; }
-                else                { std::swap(*p, *mbr->pboolVal); }
-                break;
-            }
-            default:
-            {
-                return 0;
-            }
+            default:                return 0;
         }
-        return -1;
     }
     else
     {
@@ -169,8 +116,8 @@ VARIANT_BOOL __stdcall copy_valueMember(IDispatch* me, VARIANT* mbr, IDispatch* 
         if ( 0 < dir )      { ::VariantCopy(p, mbr); }
         else if ( dir < 0 ) { ::VariantCopy(mbr, p); }
         else                { std::swap(*p, *mbr); }
-        return -1;
     }
+    return -1;
 }
 
 // VBAのクラスオブジェクトのオブジェクト型のメンバを同一クラスの他のオブジェクトにコピーする
@@ -189,77 +136,31 @@ VARIANT_BOOL __stdcall copy_objectMember(IDispatch* me, IDispatch** pmbr, IDispa
     }
     else
     {
-        BSTR name = [](VARIANT* nm)->BSTR {
-            if ( nm->vt & VT_BYREF )
-                return ( (nm->vt & VT_BSTR) && nm->pbstrVal )? *nm->pbstrVal : nullptr;
-            else
-                return ( (nm->vt & VT_BSTR) && nm->bstrVal )?   nm->bstrVal  : nullptr;
-        } (method);
+        BSTR name = (method->vt & VT_BYREF )?
+                ( ((method->vt & VT_BSTR) && method->pbstrVal )? *method->pbstrVal : nullptr ) :
+                ( ((method->vt & VT_BSTR) && method->bstrVal ) ? method->bstrVal  : nullptr );
         if ( !name )    return 0;
         VARIANT tmp;
         ::VariantInit(&tmp);
         tmp.vt = VT_DISPATCH;
-        tmp.pdispVal = NULL;
+        tmp.pdispVal = nullptr;
         DISPPARAMS dp = { nullptr, nullptr, 0, 0 };
-        if ( 0 < dir )
+        if (IDispatch*& from_ = (0 < dir) ? *pmbr : *p )
         {
-            if ( *pmbr )
-            {
-                DISPID dispid;
-                (*pmbr)->GetIDsOfNames(IID_NULL, &name, 1, LOCALE_USER_DEFAULT, &dispid);
-                HRESULT hr = (*pmbr)->Invoke(dispid,
-                                             IID_NULL,
-                                             LOCALE_SYSTEM_DEFAULT,
-                                             DISPATCH_METHOD,
-                                             &dp,
-                                             &tmp,
-                                             NULL,
-                                             NULL);
-                if ( hr != S_OK )  return 0;
-            }
-            std::swap(tmp.pdispVal, *p);
+            DISPID dispid;
+            from_->GetIDsOfNames(IID_NULL, &name, 1, LOCALE_USER_DEFAULT, &dispid);
+            HRESULT hr = from_->Invoke(dispid,
+                                        IID_NULL,
+                                        LOCALE_SYSTEM_DEFAULT,
+                                        DISPATCH_METHOD,
+                                        &dp,
+                                        &tmp,
+                                        NULL,
+                                        NULL);
+            if (hr != S_OK)  return 0;
         }
-        else
-        {
-            if ( *p )
-            {
-                DISPID dispid;
-                (*p)->GetIDsOfNames(IID_NULL, &name, 1, LOCALE_USER_DEFAULT, &dispid);
-                HRESULT hr = (*p)->Invoke(dispid,
-                                          IID_NULL,
-                                          LOCALE_SYSTEM_DEFAULT,
-                                          DISPATCH_METHOD,
-                                          &dp,
-                                          &tmp,
-                                          NULL,
-                                          NULL);
-                if ( hr != S_OK )  return 0;
-            }
-            std::swap(tmp.pdispVal, *pmbr);
-        }
+        std::swap(tmp.pdispVal, (0 < dir) ? *p: *pmbr);
         ::VariantClear(&tmp);
-    }
-    return -1;
-}
-
-namespace   {
-    //ReDim可能な配列かどうか
-    HRESULT redimCheck(SAFEARRAY * psa)
-    {
-        const auto dim = ::SafeArrayGetDim(psa);
-        if ( 0 < dim )
-        {
-            SAFEARRAYBOUND saboundNew{0, 0};
-            LONG lLbound, lUbound;
-            auto a1 = ::SafeArrayGetLBound(psa, dim, &lLbound);
-            auto a2 = ::SafeArrayGetUBound(psa, dim, &lUbound);
-            saboundNew.cElements = 1 + lUbound - lLbound;
-            saboundNew.lLbound = lLbound;
-            return ::SafeArrayRedim(psa, &saboundNew);
-        }
-        else
-        {
-            return S_OK;    //未初期化配列もReDim可能
-        }
+        return -1;
     }
 }
